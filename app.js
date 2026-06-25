04b0d89c32e96e26e2d00dbcae6ab26c87049ca4
Aryan <aryankundalwal1630@gmail.com>
2026-06-25 18:00:37 +0530

    Completed Blogify project

---
+++ app.js
@@ -0,0 +1,52 @@
+require("dotenv").config();
+
+const express = require("express")
+const path = require("path")
+const userRoute = require('./routes/user')
+const blogRoute = require('./routes/blog')
+const mongoose = require("mongoose")
+const cookieParser = require('cookie-parser')
+const { checkForAuthenticationCookie } = require("./middlewares/authentication")
+const Blog =require("./models/blog")
+const User = require("./models/user")
+
+
+const app = express()
+const PORT = process.env.PORT || 8000;
+
+
+mongoose.connect(process.env.MONGO_URL)                     //'mongodb://localhost:27017/blogify'
+.then((e) => console.log("MongoDB connected !"))
+ .catch(err => console.log("❌ MongoDB connection error:", err));
+
+app.set("view engine" , "ejs")
+app.set("views" ,path.resolve("./views"))
+
+app.use(express.urlencoded({extended : false}))
+app.use(express.json());
+app.use(cookieParser());
+app.use(checkForAuthenticationCookie("token"))
+
+app.use(express.static(path.resolve('./public')))
+
+
+
+
+app.get("/" , async (req , res) => {
+     const allBlogs = await Blog.find({})
+     
+    
+    return res.render("home",{
+         user : req.user,
+         blogs : allBlogs,
+       
+        
+    })
+  
+   
+})
+
+app.use("/user" , userRoute)
+app.use("/blog" , blogRoute)
+
+app.listen(PORT , ()=>(console.log(`Server started at PORT : ${PORT}`)))
