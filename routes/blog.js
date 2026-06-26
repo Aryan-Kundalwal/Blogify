4a762dec7d7bd683ba79a2914b1ae4f969ad453e
Aryan <aryankundalwal1630@gmail.com>
2026-06-26 11:34:37 +0530

    fix: cloudinary image upload for vercel deployment

--- routes/blog.js
+++ routes/blog.js
@@ -1,24 +1,11 @@
 const {Router} = require("express")
-const multer =require("multer")
-const path = require("path")
 const Blog = require("../models/blog")
 const Comment = require("../models/comment")
 const { connectTOMongoDB } = require("../connect");
+const upload = require("../middlewares/upload");
 
 const router = Router()
 
-const storage = multer.diskStorage({
-    destination : function(req , file , cb){
-    cb(null,path.resolve("./public/uploads/"))
-    },
-
-     filename : function(req , file , cb){
-    const fileName = `${Date.now()}-${file.originalname}`
-    cb(null,fileName)
-    }
-})
-
-const upload = multer({storage:storage})
 
 router.get('/add-new' ,(req,res) =>{
     return res.render('addBlog',{
@@ -55,7 +42,7 @@ router.post('/' ,upload.single("coverImage"),async (req,res) =>{
    body , 
    title,
    createdBy :req.user._id,
-   coverImageURL : `/uploads/${req.file.filename}`
+   coverImageURL: req.file.path
    })
     return res.redirect(`/blog/${blog._id}`)
 })
